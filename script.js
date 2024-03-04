e2600c171c200431970ccc6bcdb15f8af010b90c
James <theanos@tutanota.com>
2024-03-04 15:04:58 +0100

    Grid header&footer

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-        const images = document.querySelectorAll("conseils img");
-        let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll("conseils img");
+    let currentIndex = 0;
 
-        function showImage(Index) {
-            images.forEach((image, i) => {
-                if (i === Index) {
-                    image.className.add('active');
-                }
-                else {
-                    image.className.remove('active');
-                }
-            });
-        };
-        
-        function imageTransition() {
-            setInterval (() => {
-                currentIndex = (currentIndex + Index) % images.length;
-                showImage(currentIndex);
-            }, 5000);
-        }
-        
-        showImage(currentIndex);
-        imageTransition();
+    function showImage(Index) {
+        images.forEach((image, i) => {
+            if (i === Index) {
+                image.className.add('active');
+            }
+            else {
+                image.className.remove('active');
+            }
+        });
+    };
+
+    function imageTransition() {
+        setInterval(() => {
+            currentIndex = (currentIndex + Index) % images.length;
+            showImage(currentIndex);
+        }, 5000);
+    }
+
+    showImage(currentIndex);
+    imageTransition();
 })
